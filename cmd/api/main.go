748c5e3849d2c90b693d51a15a2fd745bf859823
Ahmad Ibrahim Shahmi <ahmadibrahim.shahmi@gmail.com>
2026-08-18 06:30:48 +0800

    Added vercel serverless functions support

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -14,25 +14,30 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	_ = godotenv.Load()
 	config.Init()
 
 	pref := tele.Settings{
-		Token:  os.Getenv("TOKEN"),
-		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+		Token: os.Getenv("TOKEN"),
+	}
+
+	if port := os.Getenv("PORT"); port != "" {
+		pref.Synchronous = true
+		pref.Poller = &tele.Webhook{
+			Listen:      ":" + port,
+			Endpoint:    &tele.WebhookEndpoint{PublicURL: "https://" + os.Getenv("VERCEL_URL") + "/webhook"},
+			SecretToken: os.Getenv("WEBHOOK_SECRET"),
+		}
+	} else {
+		pref.Poller = &tele.LongPoller{Timeout: 10 * time.Second}
 	}
 
 	b, err := tele.NewBot(pref)
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 
 	appwrite.Init()
 	handlers.HandleButtons(b)
-
 	b.Start()
 }
